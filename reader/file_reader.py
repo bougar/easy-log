f243c0aa146d6c0bacc7957eef617439245fc5b9
Carlos Bouzón García <carlos.bouzon.garcia@gmail.com>
2024-10-18 23:26:30 +0200

    Add support for multi-line

--- reader/file_reader.py
+++ reader/file_reader.py
@@ -6,29 +6,51 @@ from typing import Iterator
 import re
 
 class SimpleFileReader(Reader):
-    def __init__(self, filename: str, pattern: str = None):
+    def __init__(self, filename: str, pattern: str = None, line_pattern: str = None):
         self.logger = logging.getLogger("SimpleFileReader")
         if not os.path.exists(filename):
             raise FileNotFoundError(f"File {filename} does not exists")
         self.filename = filename
         self.pattern = pattern
+        self.line_pattern = line_pattern
         self._closed = False
+    
+    def process_buffer(self, buffer: list) -> LogEntry:
+        lines = "".join(buffer)
+        self.logger.debug(f"Reading: {lines}")
+        match = re.match(self.pattern, lines, re.DOTALL)
+        if match:
+            return LogEntry(data=match.groupdict())
+        else:
+            return LogEntry()
+
 
     def read(self) -> Iterator[LogEntry]:
         if (self._closed):
             raise ValueError("Reader is closed")
+        buffer = []
         with open(self.filename, 'r') as _file:
-            for line in _file:
+            while True:
                 if (self._closed):
                     self.logger.info("SimpleFileReader closed")
                     break
-                self.logger.debug(f"Reading: {line}")
-                match = re.match(self.pattern, line, re.DOTALL)
-                if match:
-                    log_entry = LogEntry(data=match.groupdict())
-                else:
-                    log_entry = LogEntry()
-                yield log_entry
+                line = _file.readline()
+                if not line and len(buffer) > 0:
+                    yield self.process_buffer(buffer)
+                if not line:
+                    buffer = []
+                    break
+                if not self.line_pattern:
+                    buffer = [line]
+                if self.line_pattern and re.match(self.line_pattern, line):
+                    if len(buffer) > 0:
+                        yield self.process_buffer(buffer)
+                    buffer = [line]
+                    continue
+                if self.line_pattern and not re.match(self.line_pattern, line):
+                    buffer.append(line)
+                    continue
+                yield self.process_buffer(buffer)
     
     def close(self):
         self.logger.info("Closing SimpleFileReader")
--- tests/reader/file_reader_test.py
+++ tests/reader/file_reader_test.py
@@ -2,7 +2,7 @@ from reader.file_reader import SimpleFileReader
 import tempfile
 
 def test_simple_file_reader_with_no_trace():
-    log_pattern = r'(?P<thread_id>\d+) \[(?P<thread_name>[^\]]+)\] (?P<level>[A-Z]+) +(?P<timestamp>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}) (?P<class_name>[^\s]+) \[\] - (?P<message>.*)'
+    log_pattern = r'(?P<thread_id>\d+) \[(?P<thread_name>[^\]]+)\] (?P<level>[A-Z]+) +(?P<timestamp>.*) (?P<class_name>[^\s]+) \[\] - (?P<message>.*)'
     log_data = '''2735 [main] INFO  2024-10-03T13:02:01.048 class.name [] - message'''
     expected_output = {
         'thread_id': '2735',
@@ -21,7 +21,8 @@ def test_simple_file_reader_with_no_trace():
     assert log_entry.data == expected_output
 
 def test_simple_file_reader_with_trace():
-    log_pattern = r'(?P<thread_id>\d+) \[(?P<thread_name>[^\]]+)\] (?P<level>[A-Z]+) +(?P<timestamp>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}) (?P<class_name>[^\s]+) \[\] - (?P<message>.*)(?:\n(?P<traceback>(?!\d+ \[).+(\n.+)*))?'
+    log_pattern = r'(?P<thread_id>\d+) \[(?P<thread_name>[^\]]+)\] (?P<level>[A-Z]+) +(?P<timestamp>.*) (?P<class_name>[^\s]+) \[\] - (?P<message>[^\n]+)(\n(?P<traceback>.*))' 
+
     log_data = '''2735 [main] INFO  2024-10-03T13:02:01.048 class.name [] - message
 Traceback (most recent call last):
   File "<stdin>", line 1, in <module>
@@ -32,13 +33,13 @@ ZeroDivisionError: division by zero'''
         'level': 'INFO',
         'timestamp': '2024-10-03T13:02:01.048',
         'class_name': 'class.name',
-        'message': 'message\n',
+        'message': 'message',
         'traceback': 'Traceback (most recent call last):\n  File "<stdin>", line 1, in <module>\nZeroDivisionError: division by zero'
     }
     temporary_file = tempfile.NamedTemporaryFile()
     with open(temporary_file.name, 'w') as file:
         file.write(log_data)
-    simple_file_reader = SimpleFileReader(temporary_file.name, log_pattern)
+    simple_file_reader = SimpleFileReader(temporary_file.name, log_pattern, line_pattern=r'(\d+).*')
     log_entry = next(simple_file_reader.read())
     simple_file_reader.close()
     assert log_entry.data == expected_output
